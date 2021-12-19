eeb6646ac77214a06d0550b50f9b7fa36eb311a6
youle921 <toshiki.urita@ci.cs.osakafu-u.ac.jp>
2021-12-19 23:51:24 +0900

    add 3d plots

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 import pathlib
 
-alg_names = [n.split("/")[0] for n in["NSGA-II/1017", "MO-MFEA/1019", "MO-MFEA-II/1019", "EMEA/1017", "Island_Model/1017"]]
-original_names = ["Four bar truss design", "Reinforced concrete beam design", "Pressure vessel design","Hatch cover design","Coil compression spring design"]
+names = ["NSGA-II/1219", "MO-MFEA/1019", "MO-MFEA-II/1019", "EMEA/1017", "Island_Model/1017"]
+alg_names = [n.split("/")[0] for n in names]
+original_names = ["Four bar truss design", "Reinforced concrete beam design", "Pressure vessel design","Hatch cover design","Coil compression spring design","Two bar truss design", "Welded beam design","Disc brake design","Vehicle crashworthiness design","Speed reducer design","Gear train design","Rocket injector design"]
 
 def show_problem_info():
     info_array = []
@@ -15,39 +16,35 @@ def show_problem_info():
         info_array.append([int(i/2) + 1, name, 2, d_])
     df = pd.DataFrame(info_array, columns = ["Problem Set No.", "Problem Name", "Number of Objectives", "Number of Dimensions"], index = np.arange(len(info_array))+1)
     
-    st.dataframe(df)
+    with st.expander("See Information"):
+        st.dataframe(df)
 
 @st.cache
 def data_loader():
-
 
     pf = {}
     objs = {}
     metrics = {}
 
     problem_names = ["RE21", "RE22", "RE23", "RE24","RE25"]
+    problem_names.extend(["RE31","RE32", "RE33", "RE34", "RE35", "RE36", "RE37"])
 
-    names = ["NSGA-II/1017", "MO-MFEA/1019", "MO-MFEA-II/1019", "EMEA/1017", "Island_Model/1017"]
+    for alg_name, n in zip(alg_names, names):
 
-    for alg_name in names:
+        objs[alg_name] = {}
+        metrics[alg_name] = {}
 
-        n = alg_name.split("/")[0]
-        objs[n] = {}
-        metrics[n] = {}
-
-        p = pathlib.Path(alg_name)
+        p = pathlib.Path(n)
         dirs = p.glob("*design/")
 
         for d in dirs:
 
             key = str(d.name).split("_")[-1]
             if key in original_names:
-                objs[n][key] = np.load(f'{alg_name}/{d.name}/objectives.npz')["arr_0"]
-                metrics[n][key] = np.loadtxt(f'{alg_name}/{d.name}/normalized_IGD.csv', delimiter = ",")
+                objs[alg_name][key] = np.load(f'{n}/{d.name}/objectives.npz')["arr_0"]
+                metrics[alg_name][key] = np.loadtxt(f'{n}/{d.name}/normalized_IGD.csv', delimiter = ",")
 
-    # original_names = ["Four bar truss design", "Reinforced concrete beam design", "Pressure vessel design","Hatch cover design","Coil compression spring design","Two bar truss design", "Welded beam design","Disc brake design","Vehicle crashworthiness design","Speed reducer design","Gear train design","Rocket injector design","Car side impact design","Conceptual marine design"]
-
-    for name, original_name in zip(problem_names, original_names):
+    for name, original_name in zip(problem_names, metrics["NSGA-II"].keys()):
 
         max_IGD = np.array([[np.max(alg_value[original_name])] for alg_value in metrics.values()]).max(axis = 0)
 
@@ -62,39 +59,82 @@ def data_loader():
         pf[original_name] = ((front[front[:, 0].argsort()] - ideal) / (nadir - ideal)).T
 
         for alg_name in alg_names:
-            objs[alg_name.split("/")[0]][original_name] = (objs[alg_name.split("/")[0]][original_name] - ideal) / (nadir - ideal)
+            objs[alg_name][original_name] = (objs[alg_name][original_name] - ideal) / (nadir - ideal)
 
     return pf, objs, metrics
 
+
+show_problem_info()
+pf, objs, metrics = data_loader()
+
+nobj = st.sidebar.radio("目的数", [2, 3])
+if nobj == 2:
+    problem = st.sidebar.selectbox("問題名", original_names[:5])
+elif nobj == 3:
+    problem = st.sidebar.selectbox("問題名", original_names[5:])
+    st.sidebar.write("表示角度")
+    z_angle = st.sidebar.slider("仰角", 0, 90, 30, 5)
+    xy_angle = st.sidebar.slider("方位角( → 時計回り)", 0, 360, 300, 10)
+
+alg = st.sidebar.radio("アルゴリズム名", alg_names)
+gen = st.sidebar.slider("世代数", 1, 50)
+
+# z_angle = 30
+# xy_angle = -60
+
+
 # @st.cache
-def first_draw(pf, metrics):
+def first_draw(nobj, pf, metrics):
 
-    p_name = "Coil compression spring design"
-    alg_name = "MO-MFEA"
+    if nobj == 2:
 
-    # setting
-    figs = dict(zip(["fig", "axes"], plt.subplots(2, 1)))
-    ax, sub_ax = figs["axes"]
-    figs["ax"] = ax
-    figs["sub_ax"] = sub_ax
+        # setting
+        p_name = "Coil compression spring design"
+        alg_name = "MO-MFEA"
 
-    # population plot
-    figs["line"], = figs["ax"].plot(pf[p_name][0], pf[p_name][1], c = "k", label = "Pareto Front")
-    figs["ax"].set_aspect('equal')
+        figs = dict(zip(["fig", "axes"], plt.subplots(2, 1)))
 
-    figs["ax"].set_title('population plot')
-    figs["scat"] = figs["ax"].scatter([], [], label = "Population")
+        ax, sub_ax = figs["axes"]
 
-    figs["ax"].legend(loc = "upper right", bbox_to_anchor = [1.9, 1])
+        figs["ax"] = ax
+
+        figs["sub_ax"] = sub_ax
+        figs["sub_ax"].set_title("IGD (scaled [0-1])")
+
+        # population plot
+        figs["line"], = figs["ax"].plot(pf[p_name][0], pf[p_name][1], c = "k", label = "Pareto Front")
+        figs["ax"].set_aspect('equal')
+
+        figs["ax"].set_title('population plot')
+        figs["scat"] = figs["ax"].scatter([], [], label = "Population")
+
+        figs["ax"].legend(loc = "upper right", bbox_to_anchor = [1.9, 1])
+
+    elif nobj == 3:
+
+        # setting
+        p_name = "Welded beam design"
+        alg_name = "MO-MFEA"
+        
+        figs = dict(fig = plt.figure())
+        figs["ax"] = figs["fig"].add_subplot(211, projection = '3d')
+        figs["line"] = figs["ax"].scatter(*pf[p_name], color = "k", alpha = 1, label = "Pareto Front")
+
+        figs["sub_ax"] = figs["fig"].add_subplot(212)
+        figs["sub_ax"].set_title("\nIGD (scaled [0-1])")
+
+        # population plot
+        figs["scat"] = figs["ax"].scatter([], [], [], label = "Population")
+        figs["ax"].set_title('population plot')
+
+        figs["ax"].legend(loc = "upper right", bbox_to_anchor = [2.0, 1])
 
     # IGD plot
-    figs["IGD_lines"] = sub_ax.plot(range(1, 51), np.array([m[p_name] for m in metrics.values()]).T, label = alg_names)
-    figs["current_gen"] = plt.axvline(0, c = "k", label = "current gen")
-    figs["sub_ax"].set_title("IGD (scaled [0-1])")
+    figs["IGD_lines"] = figs["sub_ax"].plot(range(1, 51), np.array([m[p_name] for m in metrics.values()]).T, label = alg_names)
+    figs["current_gen"] = plt.axvline(0, c = "k", label = "current gen", zorder = 3)
 
     figs["sub_ax"].legend()
     figs["sub_ax"].grid()
-
     plt.tight_layout()
 
     return figs
@@ -104,7 +144,7 @@ class image_viewer:
     def __init__(self):
 
         self.color_palette = {}
-        names = ["NSGA-II/1017", "MO-MFEA/1019", "MO-MFEA-II/1019", "EMEA/1017", "Island_Model/1017"]
+        names = ["NSGA-II/1219", "MO-MFEA/1019", "MO-MFEA-II/1019", "EMEA/1017", "Island_Model/1017"]
 
         self.previous = None
         for alg_name, c in zip(names, ['tab:blue','tab:orange','tab:green','tab:red','tab:purple']):
@@ -112,39 +152,49 @@ class image_viewer:
             n = alg_name.split("/")[0]
             self.color_palette[n] = c
 
-    def view_image(self, figs, pf, objs, metrics, gen, p_name = "Coil compression spring design", alg_name = "MO-MFEA"):
+    def view_image(self, nobj, figs, pf, objs, metrics, gen, p_name = "Coil compression spring design", alg_name = "MO-MFEA"):
 
-        figs["line"].set_data(pf[p_name][0], pf[p_name][1])
-        figs["scat"].set_offsets(objs[alg_name][p_name][gen - 1])
+        if nobj == 2:
+            self.view_2d_image(figs, pf, objs, metrics, gen, p_name, alg_name)
+        elif nobj == 3:
+            self.view_3d_image(figs, pf, objs, metrics, gen, p_name, alg_name)
 
         for l, key in zip(figs["IGD_lines"], objs.keys()):
 
             if key == alg_name:
                 l.set_data(range(1, 51), metrics[key][p_name])
-                l.zorder = 5
+                l.zorder = 10
             else:
                 l.set_data(range(1, 51), metrics[key][p_name])
-                l.zorder = -1
+                l.zorder = 3
                 l.set_linestyle("dashed")
                 l.set_linewidth(1)
 
-#         self.IGD_lines.set_data(range(1, 51), self.metrics[alg_name][p_name])
-#         self.IGD_lines.set_color(self.color_palette[alg_name])
-#         self.current_point.sef_offsets(gen, self.metrics[alg_name][p_name][gen - 1])
         figs["current_gen"].set_xdata(gen)
 
-show_problem_info()
+    def view_2d_image(self, figs, pf, objs, metrics, gen, p_name = "Coil compression spring design", alg_name = "MO-MFEA"):
 
-pf, objs, metrics = data_loader()
+        figs["line"].set_data(pf[p_name][0], pf[p_name][1])
+        figs["scat"].set_offsets(objs[alg_name][p_name][gen - 1])
+
+    def view_3d_image(self, figs, pf, objs, metrics, gen, p_name = "Welded beam design", alg_name = "MO-MFEA"):
+
+        figs["line"].remove()
+        figs["scat"].remove()
+
+        mask = ((objs[alg_name][p_name][gen - 1] < -0.05) | (objs[alg_name][p_name][gen - 1] > 1.05)).max(axis = 1)
+
+        figs["scat"] = figs["ax"].scatter(*objs[alg_name][p_name][gen - 1][~mask].T, color = "tab:blue", alpha = 1)
+        figs["line"] = figs["ax"].scatter(*pf[p_name], color = "k", alpha = 1, s = 1, marker = "s")
+
+        figs["ax"].set(xlim = [-0.05, 1.05], ylim = [-0.05, 1.05], zlim = [-0.05, 1.05])
+        figs["ax"].view_init(elev = z_angle, azim = xy_angle)
+
 
 viewer = image_viewer()
 
-problem = st.sidebar.selectbox("問題名", original_names)
-alg = st.sidebar.radio("アルゴリズム名", alg_names)
-gen = st.sidebar.slider("世代数", 1, 50)
-
-fig_dict = first_draw(pf, metrics)
-viewer.view_image(fig_dict, pf, objs, metrics, gen, problem, alg)
+fig_dict = first_draw(nobj, pf, metrics)
+viewer.view_image(nobj, fig_dict, pf, objs, metrics, gen, problem, alg)
 
 st.write(fig_dict["fig"])
 
